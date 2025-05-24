df05ec5ddf33f73fe473fbd8e51a370664546300
Ben Stone <1490287+benstone@users.noreply.github.com>
2025-05-24 16:41:06 +1000

    Add SDL keycode defines to keys.h

--- kauai/src/keys.h
+++ kauai/src/keys.h
@@ -18,6 +18,21 @@
 
 #define vkNil 0
 
+#if defined(KAUAI_SDL)
+#define kvkLeft SDLK_LEFT
+#define kvkRight SDLK_RIGHT
+#define kvkUp SDLK_UP
+#define kvkDown SDLK_DOWN
+#define kvkHome SDLK_HOME
+#define kvkEnd SDLK_END
+#define kvkPageUp SDLK_PAGEUP
+#define kvkPageDown SDLK_PAGEDOWN
+#define kvkBack SDLK_BACKSPACE
+#define kvkDelete SDLK_DELETE
+#define kvkReturn SDLK_RETURN
+
+#elif defined(KAUAI_WIN32)
+
 #define kvkLeft MacWin(0x1C, VK_LEFT)
 #define kvkRight MacWin(0x1D, VK_RIGHT)
 #define kvkUp MacWin(0x1E, VK_UP)
@@ -30,5 +45,7 @@
 #define kvkBack MacWin(0x08, VK_BACK)
 #define kvkDelete MacWin(0x7F, VK_DELETE)
 #define kvkReturn MacWin(0x0D, VK_RETURN)
+
+#endif
 
 #endif //! KEYS_H
