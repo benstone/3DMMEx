3d43134540242d0548eaaa114a3e8d3a35c50543
Ben Stone <1490287+benstone@users.noreply.github.com>
2025-07-22 16:33:06 +1000

    Add virtual key constants for function keys and alpha keys

--- kauai/src/keys.h
+++ kauai/src/keys.h
@@ -31,6 +31,21 @@
 #define kvkDelete SDLK_DELETE
 #define kvkReturn SDLK_RETURN
 
+#define kvkF1 SDLK_F1
+#define kvkF2 SDLK_F2
+#define kvkF3 SDLK_F3
+#define kvkF4 SDLK_F4
+#define kvkF5 SDLK_F5
+#define kvkF6 SDLK_F6
+#define kvkF7 SDLK_F7
+#define kvkF8 SDLK_F8
+#define kvkF9 SDLK_F9
+#define kvkF10 SDLK_F10
+#define kvkF11 SDLK_F11
+#define kvkF12 SDLK_F12
+
+#define VK_FROM_ALPHA(ch) (ch - 'A' + SDLK_a)
+
 #elif defined(KAUAI_WIN32)
 
 #define kvkLeft MacWin(0x1C, VK_LEFT)
@@ -60,5 +75,7 @@
 #define kvkF12 VK_F12
 
 #define VK_FROM_ALPHA(ch) (ch)
+
+#endif
 
 #endif //! KEYS_H
