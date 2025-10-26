770bcede5225521ecbb3be72a45eb3a6578b1acd
Ben Stone <1490287+benstone@users.noreply.github.com>
2025-10-26 11:17:40 +1100

    Add cross-platform portfolio support using Native File Dialog Extended

---
+++ cmake/FetchNativeFileDialogExtended.cmake
@@ -0,0 +1,10 @@
+include(FetchContent)
+
+FetchContent_Declare(
+    nativefiledialog-extended
+    GIT_REPOSITORY https://github.com/btzy/nativefiledialog-extended
+)
+
+FetchContent_MakeAvailable(
+    nativefiledialog-extended
+)
