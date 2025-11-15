e45fb006c28a525a51dc45188fef0bee4aa4826e
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2025-11-15 22:04:19 +0000

    kauai/src: move DtsCaret() to platwin.cpp
    At the same time remove the Mac part of the MacWin() macro since this is a
    Windows-only platform file.

--- kauai/src/platwin.cpp
+++ kauai/src/platwin.cpp
@@ -69,3 +69,17 @@ uint32_t TsCurrentSystem(void)
     return 0;
 #endif
 }
+
+uint32_t DtsCaret(void)
+{
+#if defined(KAUAI_WIN32)
+    return GetCaretBlinkTime();
+#elif defined(KAUAI_SDL)
+    // Return the default caret blink time on Windows
+    const uint32_t kdtsCaret = 530; // milliseconds
+    return kdtsCaret;
+#else
+    RawRtn();
+    return 0;
+#endif
+}
