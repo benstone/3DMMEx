3abb00539f09964ecdc6ccf0653681a24edf4396
Ben Stone <1490287+benstone@users.noreply.github.com>
2026-06-28 17:06:00 +1000

    sdl: Download soundfont on all platforms

--- cmake/FetchFluidSynth.cmake
+++ cmake/FetchFluidSynth.cmake
@@ -14,14 +14,6 @@ if (MSVC)
     )
     FetchContent_MakeAvailable(FluidSynth)
     set(FluidSynth_ROOT "${fluidsynth_SOURCE_DIR}")
-
-    FetchContent_Declare(
-        SoundFont
-        URL "https://github.com/musescore/MuseScore/raw/refs/tags/v4.7.1/share/sound/MS%20Basic.sf3"
-        URL_HASH SHA256=5EA2375E8BD7D8E71DEF1036978C1621E85B66934169B6A2744B27B9B3C2D99C
-        DOWNLOAD_NAME "soundfont.sf3"
-        DOWNLOAD_NO_EXTRACT ON
-    )
-    FetchContent_MakeAvailable(SoundFont)
-    set(SoundFont_ROOT "${soundfont_SOURCE_DIR}")
+else()
+    message(WARNING "Cannot download FluidSynth for this platform/compiler")
 endif()
